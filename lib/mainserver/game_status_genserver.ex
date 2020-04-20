10ab209d20c4b4d123c2e7094ce84d689535dfd7
McKinnin <lloydmckinnin@gmail.com>
2020-04-20 09:48:29 -0600

    Fixed my genserver. Need to set up the handle_call and cast

--- lib/mainserver/game_status_genserver.ex
+++ lib/mainserver/game_status_genserver.ex
@@ -1,13 +1,23 @@
 defmodule GameStatusGenServer do
   use GenServer
 
-  # def start_link(status, time_stamp, number_stamp) do
-  #   GenServer.start_link(__MODULE__, false, -1, -1)
-  # end
+  alias Models.GameStatus
+  def start_link() do
+    GenServer.start_link(__MODULE__, {false, -1, -1}, name: __MODULE__)
+  end
 
 @impl true
-  def init(status, time_stamp, number_stamp) do
-      {:ok, %{status: false, time_stamp: -1, number_stamp: -1}}
+  def init({status, time_stamp, number_rounds}) do
+      {:ok, %GameStatus{status: status, time_stamp: time_stamp, number_rounds: number_rounds}}
   end
+
+  def handle_call() do
+    #find out what needs to be waiting on and what does not. 
+  end
+
+  def handle_cast() do
+
+  end
+
 
 end
