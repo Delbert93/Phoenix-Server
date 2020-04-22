0a4ea94067a14fdc47982a36beb49ac8e88a02b7
Ammon Zerkle <ammonzerkle@gmail.com>
2020-04-22 08:47:03 -0600

    Worked on converting the contestant genserver to a scoreboard genserver

--- lib/gameServer_web/contestant.ex
+++ lib/gameServer_web/contestant.ex
@@ -1,5 +1,5 @@
 defmodule Contestant do
   # ~w will create a list of words, the a at the end means it will actually create a list of atoms.
   # this is the same thing as saying defstruct [:url, :title, :posted_timestamp, :source]
-  defstruct ~w(Name ContestantID)a
+  defstruct ~w(Name Round Maze Start Exit Score)a
 end
--- lib/gameServer_web/server/contestants.ex
+++ lib/gameServer_web/server/contestants.ex
@@ -10,7 +10,37 @@ defmodule Contestants do
   end
 
   def add_contestant(contestant) do
-    GenServer.cast(__MODULE__, {:add_contestant, {contestant, "1a2b3c"}})
+    GenServer.cast(__MODULE__, {:add_contestant, {contestant, 0, [], [], [], 0}})
+    :ets.lookup(table_name(), contestant)
+  end
+
+  def score_contestant(score_contestant = %Contestant{}) do
+    round = 0 #variable not needed for production, remove when merged with Main genserver
+    maze_width = 1
+    maze_height = 1
+
+    #Checks if the contestant is in the ETS table
+    case :ets.lookup(table_name(), score_contestant.name) do
+      [_] ->
+        #Checks to see if the contestants round is the same as the current round
+        cond do
+          score_contestant.round == round #Replace with Maingenserver.get_round()
+          ->
+                #Takes the existing score for the contestant and adds teh points gotten for the current round
+                #After it does that it creates a new contestant variable and sends it to the score_contestant cast
+                new_score = (score_contestant.score + 1) #(Pathfinder.path_length(score_contestant.maze, score_contestant.start, score_contestant.exit))
+                temp_cont = {score_contestant.name, score_contestant.round, score_contestant.maze, score_contestant.start, score_contestant.exit, new_score}
+                GenServer.cast(__MODULE__, {:score_contestant, temp_cont})
+          true
+          ->
+                #Takes the existing score for the contestant and adds a set amount of points
+                #After it does that it creates a new contestant variable and sends it to the score_contestant cast
+                new_score = (score_contestant.score + (maze_width * maze_height))
+                temp_cont = {score_contestant.name, score_contestant.round, score_contestant.maze, score_contestant.start, score_contestant.exit, new_score}
+                GenServer.cast(__MODULE__, {:score_contestant, temp_cont})
+          end
+      [] -> nil
+    end
   end
 
   def get_contestants() do
@@ -21,6 +51,7 @@ defmodule Contestants do
 
   @impl true
   def init(_) do
+    #Initializes a new local ETS table
     :ets.new(table_name(), [:named_table, :set, :protected])
     {:ok, nil}
   end
@@ -28,9 +59,20 @@ defmodule Contestants do
 
   @impl true
   def handle_cast({:add_contestant, new_contestant = %Contestant{}}, _) do
+    #Checks if the contestant exists in the table already
     case :ets.lookup(table_name(), new_contestant.name) do
-      [] -> :ets.insert(table_name(), {new_contestant.name, })
+      [] -> :ets.insert(table_name(), {new_contestant.name, new_contestant.round, new_contestant.maze, new_contestant.start, new_contestant.exit, new_contestant.score})
       _ -> nil
+    end
+
+    {:noreply, nil}
+  end
+
+  @impl true
+  def handle_cast({:score_contestant, contestant = %Contestant{}}, _) do
+    case :ets.lookup(table_name(), contestant.name) do
+      [_] -> :ets.insert(table_name(), {contestant.name, contestant.round, contestant.maze, contestant.start, contestant.exit,  contestant.score})
+      [] -> nil
     end
 
     {:noreply, nil}
